e27c3c4ffe1557159c1b676d6bd3aa2a9fc72e9c
Nastya Izotina <nastya.izotina@mail.ru>
2021-10-12 00:07:25 +0300

    3 commit and the end part1

--- src/main/java/ru/mirea/ikbo1319/game_pattern/controller/DefaultController.java
+++ src/main/java/ru/mirea/ikbo1319/game_pattern/controller/DefaultController.java
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.mirea.ikbo1319.game_pattern.entity.Game;
 import ru.mirea.ikbo1319.game_pattern.repo.GameRepo;
@@ -21,6 +19,9 @@ public class DefaultController {
 
     int id = 0;
 
+    int counter = 0;
+    int cnt=1;
+
     @GetMapping("/")
     public String index(Model model){
         Iterable<Game> games = gameRepo.findAll();
@@ -33,26 +34,34 @@ public class DefaultController {
         ArrayList<Game> games = gameRepo.findAllByIsUmlIsTrue();
         Game that_game = games.get(id);
         model.addAttribute("that_game", that_game);
-        return "onlyImages";
+        return "/onlyImages";
     }
 
-    @PostMapping(value = "/answer_only_images")
-    public String answerOnlyImages(Model iA, Boolean that_answer){
-        iA.addAttribute("is_answer", that_answer);
+    @GetMapping(value = "/answer_only_images")
+    public String answerOnlyImages(@RequestParam Long idGame){
+
         ArrayList<Game> games = gameRepo.findAllByIsUmlIsTrue();
         Game that_game = games.get(id);
-        System.out.println(that_answer);
-        //Vector<Boolean> answers = new Vector<Boolean>();
-        //answers.add(0, false);
-        //answers.add(1,that_game.getIsAnswer1Right());
-        //answers.add(2,that_game.getIsAnswer2Right());
-        //answers.add(3,that_game.getIsAnswer3Right());
-        //answers.add(4,that_game.getIsAnswer4Right());
-        //Boolean that_answer = answers.get(Math.toIntExact(i));
 
-        //System.out.println(i);
-        //System.out.println(that_answer);
+        Vector<Boolean> answers = new Vector<>();
+        answers.add(0,that_game.getIsAnswer1Right());
+        answers.add(1,that_game.getIsAnswer2Right());
+        answers.add(2,that_game.getIsAnswer3Right());
+        answers.add(3,that_game.getIsAnswer4Right());
+        Boolean that_answer = answers.get(Math.toIntExact(idGame));
+
+        if(that_answer)
+            counter++;
+
+        cnt++;
         id++;
+        System.out.println(cnt);
+        if(cnt == games.size()-1){
+            cnt=1;
+            answers.clear();
+            id=0;
+            return "redirect:/ending";
+        }
         return "redirect:/onlyImages";
     }
 
